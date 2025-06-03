9eda672188cafafa97777a73879e514ab29381ae
sanjaykumar06-10 <thallapallisanjaykumar06@gmail.com>
2025-06-03 11:33:22 +0530

    updated

--- index.js
+++ index.js
@@ -1,6 +1,19 @@
 import express from "express";
+import mongoose from "mongoose";
 import cors from "cors";
 const app = express();
+app.listen(8080,()=>{
+    mongoose.connect("mongodb://localhost:27017/gcet");
+    console.log("server started in port 8080");
+});
+const userSchema=mongoose.Schema({
+  name :{type :String},
+});
+const user=mongoose.model("User",userSchema);
+app.get("register", async (req, res) => {
+  const result = await user.insertOne({name: "John"});
+  return res.json(result);
+});
 app.listen(8080, () => {
   console.log("Server Started");
 });
@@ -8,6 +21,7 @@ app.use(cors());
 app.get("/", (req, res) => {
   return res.send("Good Morning");
 });
+
 
 app.get("/greet", (req, res) => {
   res.send("Greetings");
@@ -26,6 +40,7 @@ app.get("/products", (req, res) => {
     { name: "Laptop", price: 34000 },
     { name: "KeyBoard", price: 1200 },
     { name: "Mouse", price: 900 },
+    
   ];
   res.json(products);
 });
