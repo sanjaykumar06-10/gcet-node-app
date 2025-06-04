2c854b9ad6e77a09c8d762dc8a870dd4096ee8af
sanjaykumar06-10 <thallapallisanjaykumar06@gmail.com>
2025-06-04 11:33:08 +0530

    UPDATE

--- index.js
+++ index.js
@@ -12,13 +12,23 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-const MONGODB_URI = process.env.MONGODB_URI
+const MONGO_URI = process.env.MONGO_URI
 
 app.use("/users", userRouter);
 app.use("/products", productRouter);
 app.use("/orders", ordersRouter);
 
-app.listen(8080, () => {
+/*app.listen(8080, () => {
   mongoose.connect(`${MONGODB_URI}`);
   console.log("Server Started");
-});
+});*/
+mongoose
+  .connect(MONGO_URI)
+  .then(() => {
+    app.listen(8080, () => {
+      console.log("Server Started on port 8080");
+    });
+  })
+  .catch((error) => {
+    console.log(error);
+  });
